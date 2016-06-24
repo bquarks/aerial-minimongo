b85c83f07a56f5687e09b1d86f9727f85868e57d
DANIEL GARCÍA <daniel.garcia@bq.com>
2016-06-24 12:47:31 +0200

    Bump to version 0.0.20

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bquarks:aerial-minimongo',
-    version: '0.0.19',
+    version: '0.0.20',
 
     // Brief, one-line summary of the package.
     summary: 'Modified version of minimongo to connect with an api rest',
